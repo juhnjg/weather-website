833669bb8e17efba726ffcc0c3c3220f8e8353aa
juhnjg <juhnjg@gmail.com>
2021-11-05 09:31:19 +0900

    destruction applied

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -4,16 +4,17 @@ const forecast = (latitude ,longitude,  callback) => {
 
     const url = 'http://api.weatherstack.com/current?access_key=585ba13b2ce1e2a5d3596b50c3346bcb&query=' + latitude+ ',' + longitude + '&units=m'
 
-    request({ url: url, json: true }, (error, response) => {
+    request({ url: url, json: true }, (error, {body}) => {
 
         if (error) {
             callback('Unable to connect to weather service!', undefined)
 
-        } else if (response.body.error) {
+        } else if (body.error) {
             callback('Unable to find location', undefined)
 
         } else {
-            callback(undefined, response.body.current.weather_descriptions[0] + ' at '+ response.body.current.observation_time+'. It is currently ' + response.body.current.temperature + ' degrees out. There is a ' + response.body.current.precip + '% chance of rain.')
+            console.log(body.current)
+            callback(undefined, body.current.weather_descriptions[0] + ' at '+ body.current.observation_time+'. It is currently ' + body.current.temperature + ' degrees out. There is a ' + body.current.precip + '% chance of rain.')
         }
 
     })
--- src/utils/geocode.js
+++ src/utils/geocode.js
@@ -3,16 +3,16 @@ const request = require('request')
 const geocode = (address, callback) => {
     const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' + encodeURIComponent(address) + '.json?access_token=pk.eyJ1IjoianVobmpnIiwiYSI6ImNrdmJrN3R1ajBncWwyb3Fnb2pkeWowOTYifQ.b5i4i8nnp3ADgPKwyNC1Zg'
 
-    request({ url: url, json: true }, (error, response) => {
+    request({ url: url, json: true }, (error, {body}) => {
         if (error) {
             callback('Unable to connect to location services', undefined)
-        } else if (Array.isArray(response.body.features) && response.body.features.length === 0) {
+        } else if (Array.isArray(body.features) && body.features.length === 0) {
             callback('Unable to find location. Try another search @geocode', undefined)
         } else {
             callback(undefined, {
-                latitude: response.body.features[0].center[1],
-                logitude: response.body.features[0].center[0],
-                location: response.body.features[0].place_name
+                latitude: body.features[0].center[1],
+                logitude: body.features[0].center[0],
+                location: body.features[0].place_name
             })
         }
     })
